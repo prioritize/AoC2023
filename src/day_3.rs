7a3888d4c205d07d6a9911b66a8b7663fe4699bc
prioritize <jared@prototypic.al>
2023-12-06 11:54:48 -0800

    eod

--- src/day_3.rs
+++ src/day_3.rs
@@ -18,17 +18,27 @@ fn day_3_part_1(fname: &str) {
     // Collect all the lines of the file into a vector
     let schematic = lines.map(|x| {
         match x {
-            Ok(x) => x,
+            Ok(x) => {x.chars().collect::<Vec<char>>()},
             Err(_) => {panic!("at the disco")}
         }
-    }).collect::<Vec<String>>();
+    }).collect::<Vec<Vec<char>>>();
     // Create a storage location for symbols
+    let mut visited = vec![vec![]];
+    for line in &schematic {
+        let mut v = vec![];
+        for c in line.iter() {
+            v.push(false);
+        }
+        visited.push(v);
+    }
 
     // Iterate the vectors to find all values that aren't numeric or periods
-
+    let mut symbols = vec![];
     for (r_idx, r)  in schematic.iter().enumerate() {
-        for (c_idx, c) in r.chars().enumerate() {
-
+        for (c_idx, c) in r.iter().enumerate() {
+            if is_symbol(*c) {
+               check_neighborhood((r_idx, c_idx), &schematic);
+            }
         }
 
     }
@@ -51,20 +61,45 @@ fn find_start_and_end(idx: usize, line: String) -> (usize, usize){
     let rev = chars.0.chars().rev().try_for_each(|x| {
         if x.is_numeric() {
             rev_offset += 1;
-            return true
+            return Ok(())
         }
-        false
+        Err(())
     });
     let mut forward_offset = 0;
     let forward = chars.1.chars().try_for_each(|x| {
         if x.is_numeric() {
             forward_offset += 1;
-            return true
+            return Ok(())
         }
-        false
+        Err(())
     });
     (0, 0)
+}
+fn check_neighborhood(loc: (usize, usize), schematic: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+    let mut neighbors = vec![];
+    for r  in -1..1 {
+        for c in -1..1 {
+            if r < 0 || r >= schematic.len() as i32 {
+                continue;
+            }
+            if c <= 0 || c >= schematic[0].len() as i32 {
+                continue;
+            }
+            if schematic[r as usize][c as usize].is_numeric() {
+                neighbors.push((r as usize, c as usize));
+            }
+        }
+    }
+    neighbors
 }
 // fn lines_to_vec(lines: Lines<BufReader<File>>) -> Vec<Vec<&str>> {
 //
 // }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_day_3_part_1() {
+        day_3_part_1("input/day_3_input.txt")
+    }
+}
