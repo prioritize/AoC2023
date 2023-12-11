a5a913618e4fd499049fec1ddcab0cdab623bec8
prioritize <jared@prototypic.al>
2023-12-11 13:14:44 -0800

    still working through day 5

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ops::Range;
@@ -16,91 +16,128 @@ fn parse_file(fname: &str) {
         .trim()
         .split(" ")
         .into_iter()
-        .map(|x| {
-            println!("{}", x);
-            x.parse().unwrap()
-        })
+        .map(|x| x.parse().unwrap())
         .collect::<Vec<u32>>();
-    println!("{:?}", seeds_word);
-    println!("{:?}", seeds_actual);
     // Throw away empty line
     lines.next();
-    let mut remaining = lines
-        .into_iter()
-        .flatten()
-        .collect::<VecDeque<String>>();
-    let mut current = vec![];
+    let mut remaining = lines.into_iter().flatten().collect::<VecDeque<String>>();
+    let mut current = VecDeque::new();
     let mut mappings = vec![];
-    println!("{:?}", remaining);
     while remaining.len() != 0 {
-       match remaining.pop_front() {
-           None => {
-               mappings.push(current.clone());
-               current.clear();
-               remaining.pop_front();
-           }
-           Some(value) => {
-               if value == "" {
-                   mappings.push(current.clone());
-                   current.clear();
-               } else {
-                   println!("{}", value);
-                   current.push(value);
-               }
-           }
-       }
+        match remaining.pop_front() {
+            None => {
+                mappings.push(current.clone());
+                current.clear();
+                remaining.pop_front();
+            }
+            Some(value) => {
+                if value == "" {
+                    mappings.push(current.clone());
+                    current.clear();
+                } else {
+                    current.push_back(value);
+                }
+            }
+        }
     }
-    println!("{:?}", mappings);
-    // parse_maps(&mut remaining);
+    let mut garden_maps: Vec<GardenMap> = mappings.iter_mut().map(|m| GardenMap::new(m)).collect();
+    let garden_maps: HashMap<String, GardenMap> = garden_maps
+        .into_iter()
+        .map(|m| (m.source.clone(), m))
+        .collect();
+    let overall_map = OverallMap{big_map:garden_maps};
+    println!("{}", overall_map.evaluate_seed("seed", 79));
+    // println!("{:?}", garden_maps.get("soil").unwrap())
 }
 
+#[derive(Debug)]
 struct GardenMap {
     source: String,
     destination: String,
     ranges: Vec<(Range<u32>, Range<u32>)>,
 }
-impl GardenMap {
-    fn new(header: String, ranges: Vec<String>) -> Self {
-        let mut header = header.split(" ");
-        let mut info = header.next().unwrap().split("-");
-        let source = info.next().unwrap();
-        // Throw away the "to"
-        info.next();
-        let dest = info.next().unwrap();
-
-        let ranges = ranges
+struct OverallMap {
+    big_map: HashMap<String, GardenMap>,
+}
+impl OverallMap {
+    fn evaluate_seed(&self, source: &str, seed: u32) -> u32 {
+        let seed_range: Vec<&(Range<u32>, Range<u32>)> = self
+            .big_map
+            .get(source)
+            .unwrap()
+            .ranges
             .iter()
-            .map(|l| {
-                let mut mapping = l.split(" ");
-                let source_start = mapping.next().unwrap().parse().unwrap();
-                let dest_start = mapping.next().unwrap().parse().unwrap();
-                let offset: u32 = mapping.next().unwrap().parse().unwrap();
-                (
-                    source_start..source_start + offset,
-                    dest_start..dest_start + offset,
-                )
-            })
-            .collect::<Vec<(Range<u32>, Range<u32>)>>();
-        let source = String::from(source);
-        let destination = String::from(dest);
-        GardenMap {
-            source,
-            destination,
-            ranges,
+            .map(|x| match x.0.contains(&seed) {
+                true => {Some(x)}
+                false => {None}
+            }).flatten()
+            .collect();
+        let mut mapped_value= 0;
+        match seed_range.len() {
+            0 => {
+               mapped_value = seed;
+                println!("Found None: {}", mapped_value);
+            }
+            1 => {
+                println!("Seed Range: {:?}", seed_range);
+                let seed_range = seed_range[0];
+                println!("Seed Range: {:?}", seed_range);
+                let offset = seed - seed_range.0.start;
+                println!("Offset: {:?}", offset);
+                mapped_value = seed_range.1.start + offset;
+            }
+            _ => panic!("shouldn't be getting more than 1")
+        }
+        println!("{}", self.big_map.get(source).unwrap().destination);
+        println!("")
+        match self.big_map.get(source).unwrap().destination.as_str() {
+            "soil" => self.evaluate_seed("soil", mapped_value),
+            "fertilizer" => self.evaluate_seed("fertilizer", mapped_value),
+            "water" => self.evaluate_seed("water", mapped_value),
+            "light" => self.evaluate_seed("light", mapped_value),
+            "temperature" => self.evaluate_seed("temperature", mapped_value),
+            "humidity" => self.evaluate_seed("humidity", mapped_value),
+            "location" => mapped_value,
+            _ => {
+                panic!("We shouldn't find ourselves here")
+            }
         }
     }
 }
-fn parse_map(section: &mut VecDeque<String>) -> GardenMap {
-    let front = section.pop_front().unwrap();
-    let mut split = front.split("-");
-    let source = split.next().unwrap().to_string();
-    let destination = split.next().unwrap().to_string();
-    let mappings: Vec<(Range<u32>, Range<u32>)> = section.iter().map(|l| {
-        let values = l.split(" ").into_iter().map(|v| {v.parse().unwrap()}).collect::<Vec<Vec<u32>>>();
-        values.iter().map(|v| {(v[0]..v[0]+v[2], v[1]..v[1]+v[2])}).collect::<Vec<(Range<u32>, Range<u32>)>>()
-    }).collect();
-
-
+impl GardenMap {
+    fn new(section: &mut VecDeque<String>) -> Self {
+        let front = section.pop_front().unwrap();
+        let mut split = front.split("-");
+        let source = split.next().unwrap().to_string();
+        split.next();
+        let destination = split.next().unwrap().split(" ").next().unwrap().to_string();
+        let mappings: Vec<(Range<u32>, Range<u32>)> = section
+            .iter()
+            .map(|l| {
+                let v = l
+                    .split(" ")
+                    .into_iter()
+                    .map(|v| v.parse().unwrap())
+                    .collect::<Vec<u32>>();
+                println!("{} {} {}", v[0], v[1], v[2]);
+                (v[1]..(v[1] + v[2]), v[0]..(v[0] + v[2]))
+            })
+            .collect();
+        GardenMap {
+            source,
+            destination,
+            ranges: mappings,
+        }
+    }
+    // fn evaluate_seed_location(&self, location: u32, source_type: &str) -> u32 {
+    //     //     Start with seed, iterate to location
+    //      match self.ranges.iter().find(|x| x.0.contains(&seed)) {
+    //          None => {}
+    //          Some(_) => {}
+    //      }
+    //
+    //     todo!()
+    // }
 }
 #[cfg(test)]
 mod tests {
