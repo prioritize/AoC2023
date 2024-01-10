9bc05c906dbce2ef58ed52133a4df9facda5e4cf
prioritize <jared@prototypic.al>
2024-01-10 00:25:09 -0800

    going to bed

--- src/day_10.rs
+++ src/day_10.rs
@@ -2,6 +2,8 @@ use std::collections::{BTreeMap, HashMap};
 use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::num::Add;
+use std::ops::Add;
 
 #[derive(Debug)]
 enum Segments {
@@ -18,6 +20,7 @@ struct Layout {
     pipes: HashMap<Posit, Segments>,
     visited: HashMap<Posit, bool>,
     starting: (usize, usize),
+    dims: (usize, usize),
 }
 impl Layout {
     fn new(fname: &str) -> Self {
@@ -27,6 +30,18 @@ impl Layout {
         //     .collect();
 
         todo!();
+    }
+    fn neighbors(&self, p: Posit) -> Vec<Posit> {
+        let offsets = vec![Posit::new(-1, 0), Posit::new(1, 0), Posit::new(0, -1), Posit::new(0, 1)];
+        let posits: Vec<Posit> = offsets.iter().map(|x| {
+            return x.clone() + p
+        }).collect().iter().filter(|x|{x.check_valid()});
+    }
+}
+impl Add for Posit {
+    type Output = (Posit);
+    fn add(self, rhs: Self) -> Self::Output {
+        Posit::new(self.r+rhs.r, self.c + rhs.c)
     }
 }
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
@@ -78,7 +93,7 @@ fn process(pipeline: HashMap<Posit, Segments>, start: Posit, dims: (i32, i32)) {
         (p.clone(), false)
     }).collect();
     loop {
-        stack.append(&mut current.neighbors(&visited, dims));
+        // stack.append(&mut current.neighbors(&visited, dims));
         *visited.get_mut(&current).unwrap() = true;
         counter+=1;
         match pipeline.get(&current) {
@@ -149,29 +164,40 @@ fn parse(fname: &str) -> (HashMap<Posit, Segments>, Posit, (i32, i32)) {
 impl Posit {
     // TODO: This function should only generate 4 possible neighbors, as diagonals aren't valid
     // TODO: Consider checking the values here and validating that the path can continue (has a valid direction/ character)
-    fn neighbors(&self, visited: &HashMap<Posit, bool>, dims: (i32, i32)) -> Vec<Posit> {
-        let mut out = vec![];
-        for r in -1..2 {
-            for c in -1..2 {
-                let r_idx = self.r + r;
-                let c_idx = self.c + c;
-                if r_idx == self.r && c_idx == self.c {
-                    continue
-                }
-                match r_idx < dims.0 && r_idx >= 0 && c_idx < dims.1 && c_idx >= 0 {
-                    true => {
-                        let p = Posit{r: r_idx, c: c_idx};
-                        match visited.get(&p).unwrap() {
-                            false => {out.push(p)}
-                            true => {}
-                        }
-                    }
-                    false => {}
-                }
-            }
+    fn check_valid(&self, dims: (i32, i32)) -> bool {
+        if self.r < 0 || self.c < 0 {
+            return false
         }
-        out
+        if self.r >= dims.0 || self.c >= dims.1 {
+            return false
+        }
+        true
     }
+    // fn neighbors(&self, visited: &HashMap<Posit, bool>, dims: (i32, i32)) -> Vec<Posit> {
+        // let mut out = vec![];
+        // let offsets = vec![(-1, 0), (1, 0)]
+        // vec![Posit::new(self.r-1, self.c), Posit::new(self.r+1, self.c), Posit::new(self.r)]
+        // for r in -1..2 {
+        //     for c in -1..2 {
+        //         let r_idx = self.r + r;
+        //         let c_idx = self.c + c;
+        //         if r_idx == self.r && c_idx == self.c {
+        //             continue
+        //         }
+        //         match r_idx < dims.0 && r_idx >= 0 && c_idx < dims.1 && c_idx >= 0 {
+        //             true => {
+        //                 let p = Posit{r: r_idx, c: c_idx};
+        //                 match visited.get(&p).unwrap() {
+        //                     false => {out.push(p)}
+        //                     true => {}
+        //                 }
+        //             }
+        //             false => {}
+        //         }
+        //     }
+        // }
+        // out
+    // }
     fn new(r: i32, c: i32) -> Self {
         Posit {r, c}
 
@@ -186,29 +212,29 @@ mod tests {
         let segments = parse("input/day_10_input.txt");
         println!("{:?}", segments.1);
     }
-    #[test]
-    fn test_neighbors() {
-        let segments = parse("input/day_10_input.txt");
-        let visited = segments.0.iter().map(|(p, s) |{
-            (p.clone(), false)
-        }).collect();
-        let p = Posit{r: 0, c: 0};
-        let n = p.neighbors(&visited, segments.2);
-        let answer = vec![Posit{r:0, c:1}, Posit{r:1, c:0}, Posit{r: 1, c:1}];
-        assert_eq!(answer, n);
-        let p = Posit::new(10, 10);
-        let n = p.neighbors(&visited, segments.2);
-        let answer = vec![
-            Posit{r: 9, c: 9}, Posit{r:9, c:10}, Posit{r:9, c:11},
-            Posit{r: 10, c: 9}, Posit{r:10, c:11},
-            Posit{r: 11, c: 9}, Posit{r:11, c:10}, Posit{r:11, c:11},
-        ];
-        assert_eq!(answer, n);
-    }
-    #[test]
-    fn test_process() {
-        let segments = parse("input/day_10_input.txt");
-        process(segments.0, segments.1, segments.2);
-
-    }
+    // #[test]
+    // fn test_neighbors() {
+    //     let segments = parse("input/day_10_input.txt");
+    //     let visited = segments.0.iter().map(|(p, s) |{
+    //         (p.clone(), false)
+    //     }).collect();
+    //     let p = Posit{r: 0, c: 0};
+    //     let n = p.neighbors(&visited, segments.2);
+    //     let answer = vec![Posit{r:0, c:1}, Posit{r:1, c:0}, Posit{r: 1, c:1}];
+    //     assert_eq!(answer, n);
+    //     let p = Posit::new(10, 10);
+    //     let n = p.neighbors(&visited, segments.2);
+    //     let answer = vec![
+    //         Posit{r: 9, c: 9}, Posit{r:9, c:10}, Posit{r:9, c:11},
+    //         Posit{r: 10, c: 9}, Posit{r:10, c:11},
+    //         Posit{r: 11, c: 9}, Posit{r:11, c:10}, Posit{r:11, c:11},
+    //     ];
+    //     assert_eq!(answer, n);
+    // }
+    // #[test]
+    // fn test_process() {
+    //     let segments = parse("input/day_10_input.txt");
+    //     process(segments.0, segments.1, segments.2);
+    //
+    // }
 }
